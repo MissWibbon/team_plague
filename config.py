87a89c9b335f1694e12cf7eeb2f7f802fd1e69e0
Michelle <wuebben.michelle@yahoo.com>
2020-04-10 23:12:56 -0500

    added crime values from chicago data api

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 blsKey = "54df41e29ded42d1ac5d1c051999fb65"
 
-# Enter your API key
-gkey = "AIzaSyBZkAChWJG7IiMODfNj5bd4_0ObMv4kupw"
+# Enter your API keys
+gkey = "AIzaSyAHKT2zxPFieUCLO7BB5l_RUJuFM5HhgUE"
